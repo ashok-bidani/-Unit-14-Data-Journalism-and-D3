22fae3597b1d772ecda1b3b76c3e6858b90d9132
Ashok Bidani <ashok.bidani.321@gmail.com>
2019-04-20 18:08:04 -0500

    updated visual

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-//set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 40, left: 40},
+// Set the dimensions and margins of the visual
+var margin = {top: 20, right: 20, bottom: 70, left: 70},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 
-//append the svg object to the body of the page
+// Append the svg object to the body of the page
 var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -12,47 +12,68 @@ var svg = d3.select("#chart").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//get the data
+// Get the data
 d3.csv("data.csv").then(function(data) {
 
-    //read the data values
+    // Read the data values
     data.forEach(function(d) {
         d.smoker = +d.smoker;
         d.heartAttack = +d.heartAttack;
+        d.stateAbbr = d.stateAbbr;
     });
 
-    //set the scales' domains and ranges
+    // Set the scales' domains and ranges
     var x = d3.scaleLinear()
         .range([0, width])
         .domain([0.9*(d3.min(data, (d) => {return d.smoker; })), 1.1*(d3.max(data, (d) => {return d.smoker; }))])
     var y = d3.scaleLinear()
         .range([height, 0])
-        .domain([0.9*(d3.min(data, (d) => {return d.heartAttack; })), 1.1*(d3.max(data, (d) => {return d.heartAttack; }))])
+        .domain([0.9*(d3.min(data, (d) => {return d.heartAttack; })), 1.1*(d3.max(data, (d) => {return d.heartAttack; }))]);
 
-    //add a circle element for each data point
+    // Add a circle element for each data point
+    circles = svg.append("g")
+                .selectAll("circle")
+                .data(data)
+                .join("circle")
+                .attr("cx", ((d) => {return x(d.smoker); }))
+                .attr("cy", ((d) => {return y(d.heartAttack); }))
+                .attr("r", 12);
+
     svg.append("g")
-        .selectAll("circle")
+        .selectAll("text")
         .data(data)
-        .join("circle")
-        .attr("cx", ((d) => {return x(d.smoker); }))
-        .attr("cy", ((d) => {return y(d.heartAttack); }))
-        .attr("r", 12);
-    
-    //add the X axis
+        .enter()
+        .append("text")
+        .attr("x", ((d) => {return x(d.smoker); }))
+        .attr("dx", "-0.65em")
+        .attr("y", ((d) => {return y(d.heartAttack); }))
+        .attr("dy", "0.4em")
+        .text((d) => {return d.stateAbbr});
+
+    // Add the X axis
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
 
-    //X axis label
+    // X axis label
     svg.append("text")
         .attr("x", width / 2)
         .attr("y", height + margin.top + 20)
-        .text("Smoker (%)")
+        .text("Smoker (%)");
 
-    //add the Y axis
+    // add the Y axis
     svg.append("g")
         .call(d3.axisLeft(y));
 
+    // Y axis label
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Heart Attack Ever (%)");
+    
     //display the chart
     return svg.node();
 });
--- assets/js/saved.js
+++ assets/js/saved.js
@@ -1,10 +1,10 @@
-//set the dimensions and margins of the graph
-var margin = {top: 20, right: 20, bottom: 40, left: 40},
+// Set the dimensions and margins of the visual
+var margin = {top: 20, right: 20, bottom: 70, left: 70},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
 
-//append the svg object to the body of the page
+// Append the svg object to the body of the page
 var svg = d3.select("#chart").append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -12,69 +12,57 @@ var svg = d3.select("#chart").append("svg")
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//get the data
-d3.csv("testdata.csv").then(function(data) {
+// Get the data
+d3.csv("data.csv").then(function(data) {
 
-    //read the data values
+    // Read the data values
     data.forEach(function(d) {
-    return {
-        smoker: +d.smoker,
-        heartAttack: +d.heartAttack};
-    })
+        d.smoker = +d.smoker;
+        d.heartAttack = +d.heartAttack;
+        d.stateAbbr = d.stateAbbr;
+    });
 
-    //set the scales' domains and ranges
+    // Set the scales' domains and ranges
     var x = d3.scaleLinear()
         .range([0, width])
-        .domain([d3.min(data, (d) => {return d.smoker; }), d3.max(data, (d) => {return d.smoker; })])
+        .domain([0.9*(d3.min(data, (d) => {return d.smoker; })), 1.1*(d3.max(data, (d) => {return d.smoker; }))])
     var y = d3.scaleLinear()
         .range([height, 0])
-        .domain([d3.min(data, (d) => {return d.heartAttack; }), d3.max(data, (d) => {return d.heartAttack; })])
+        .domain([0.9*(d3.min(data, (d) => {return d.heartAttack; })), 1.1*(d3.max(data, (d) => {return d.heartAttack; }))]);
 
-    //add a circle element for each data point
+    // Add a circle element for each data point
     svg.append("g")
         .selectAll("circle")
         .data(data)
         .join("circle")
         .attr("cx", ((d) => {return x(d.smoker); }))
         .attr("cy", ((d) => {return y(d.heartAttack); }))
-        .attr("r", 10);
-    
-    //add the X axis
+        .attr("r", 12);
+
+    // Add the X axis
     svg.append("g")
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
 
-    //add the Y axis
+    // X axis label
+    svg.append("text")
+        .attr("x", width / 2)
+        .attr("y", height + margin.top + 20)
+        .text("Smoker (%)");
+
+    // add the Y axis
     svg.append("g")
         .call(d3.axisLeft(y));
 
+    // Y axis label
+    svg.append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 0 - margin.left)
+        .attr("x", 0 - (height / 2))
+        .attr("dy", "1em")
+        .style("text-anchor", "middle")
+        .text("Heart Attack Ever (%)");
+    
     //display the chart
     return svg.node();
 });
-
--------------------
-
-var dataset = [[5, 10], [10, 49], [15, 30], [20, 27], [25, 120], [30, 86]];
-
-var xScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function(d) {return d[0]; })])
-    .range([0, width])
-    
-var yScale = d3.scaleLinear()
-    .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-    .range([ height, 0 ]);
-    
-svg.append("g")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-    
-svg.append("g")
-    .call(d3.axisLeft(y));
-    
-svg.selectAll("circle")
-    .data(dataset)
-    .enter()
-    .append("circle")
-    .attr("cx", function (d) {return xScale(d[0]); })
-    .attr("cy", function (d) {return yScale(d[1]); })
-    .attr("r", 10); 
